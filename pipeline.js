3538050872874c434441470ee151e48073c7ca5d
kovaaalex <kovalenko.alex04@gmail.com>
2024-06-06 14:28:13 +0300

    four

--- pipeline.js
+++ pipeline.js
@@ -1,24 +1,24 @@
 fetch('https://api.pipedrive.com/v1/persons?api_token=038f7961f6ec8877beb2b42e03903d148f877387')
-  .then(response => response.json())
-  .then(data => {
-    // Проверяем, что запрос был успешным и данные присутствуют
-    if (data.success && data.data && data.data.length > 0) {
-      // Получаем первый элемент из массива data
-      const firstPerson = data.data[0];
-      // Получаем email из объекта owner_id
-      const email = firstPerson.owner_id.email;
-      console.log('Email:', email);
-
-      // Автоматически заполняем поле email на странице, если оно существует
-      const emailInput = document.querySelector('input[type="email"]');
-      if (emailInput) {
-        emailInput.value = email;
-      } else {
-        console.error('Поле email не найдено на странице.');
-      }
-    } else {
-      console.error('Данные о клиентах не найдены или запрос не был успешным.');
-    }
+    .then(response => response.json())
+    .then(data => {
+        if (data.success && data.data && data.data.length > 0) {
+            const firstPerson = data.data[0]
+            const email = firstPerson.owner_id.email
+            console.log('Email:', email)
+            const emailInput = document.querySelector('input[type="email"]')
+            if (emailInput) 
+                emailInput.value = email
+            else
+                console.error('Поле email не найдено на странице.')
+    } else 
+      console.error('Данные о клиентах не найдены или запрос не был успешным.')
   })
-  .catch(error => console.error('Ошибка получения данных:', error));
+    .catch(error => console.error('Ошибка получения данных:', error))
+const createJob = document.querySelector('.btn__create')
 
+createJob.addEventListener('click', creatingJob)
+
+function creatingJob(){
+    createJob.innerText = "Request is send"
+    createJob.style.backgroundColor = "red"
+}
